129ff35a48964865485ca4d4ce8d853397c1b478
taslimy <taslimer@gmail.com>
2019-09-18 18:51:01 -0400

    forgot to push day 1. and pushing day 2

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,8 +1,3 @@
-import sys
-sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
-
 class BinarySearchTree:
   def __init__(self, value):
     self.value = value
@@ -10,13 +5,44 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if value < self.value:
+      if not self.left:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
+    else:
+       if not self.right:
+            self.right = BinarySearchTree(value)
+       else: 
+             self.right.insert(value)     
 
   def contains(self, target):
-    pass
+    if self.value == target:
+        return True
+    if target < self.value:
+      if not self.left:
+        return False
+      else: 
+        return self.left.contains(target)
+    else: 
+      if not self.right:
+        return False
+      else:
+        return self.right.contains(target)
+
+
 
   def get_max(self):
-    pass
+    if not self:
+      return None
+    if not self.right:
+      return self.value
+    return self.right.get_max()
 
   def for_each(self, cb):
-    pass
+    cb(self.value)
+
+    if self.left:
+        self.left.for_each(cb)
+    if self.right:
+      self.right.for_each(cb)
